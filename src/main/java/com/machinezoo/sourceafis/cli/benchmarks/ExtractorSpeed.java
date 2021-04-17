3fd1ebb52ae7d28aa4fadeb8a983040fbb027b06
Robert Važan <robert.vazan@tutanota.com>
2021-04-17 18:00:46 +0000

    Avoid dead code elimination

--- src/main/java/com/machinezoo/sourceafis/cli/benchmarks/ExtractorSpeed.java
+++ src/main/java/com/machinezoo/sourceafis/cli/benchmarks/ExtractorSpeed.java
@@ -2,24 +2,36 @@
 package com.machinezoo.sourceafis.cli.benchmarks;
 
 import java.nio.file.*;
+import java.util.*;
+import org.slf4j.*;
 import com.machinezoo.sourceafis.*;
+import com.machinezoo.sourceafis.cli.outputs.*;
 import com.machinezoo.sourceafis.cli.samples.*;
 import com.machinezoo.sourceafis.cli.utils.cache.*;
+import one.util.streamex.*;
 
 public class ExtractorSpeed extends SpeedBenchmark {
+	private static final Logger logger = LoggerFactory.getLogger(ExtractorSpeed.class);
 	@Override
 	protected TimingStats measure() {
 		return Cache.get(TimingStats.class, Paths.get("benchmarks", "speed", "extractor"), Paths.get("sample"), () -> {
 			var fingerprints = Fingerprint.all();
+			var templates = StreamEx.of(fingerprints).toMap(TemplateCache::load);
 			var recorder = new TimingRecorder(System.nanoTime(), DURATION, SAMPLE_SIZE);
+			boolean nondeterministic = false;
 			while (true) {
 				for (var fp : fingerprints) {
 					var image = fp.decode();
 					long start = System.nanoTime();
-					new FingerprintTemplate(image);
+					var template = new FingerprintTemplate(image);
 					long end = System.nanoTime();
-					if (!recorder.record(fp.dataset, start, end))
+					if (!Arrays.equals(templates.get(fp), template.toByteArray()))
+						nondeterministic = true;
+					if (!recorder.record(fp.dataset, start, end)) {
+						if (nondeterministic)
+							logger.warn("Non-deterministic extractor.");
 						return recorder.complete();
+					}
 				}
 			}
 		});
