a6971b4fef5bae14942b69b93a32e383491930c0
Robert Važan <robert.vazan@tutanota.com>
2021-12-26 02:08:42 +0000

    Java module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,25 @@
+// Part of SourceAFIS CLI for Java: https://sourceafis.machinezoo.com/cli
+/*
+ * Open the module for unfettered serialization of persistent caches.
+ * It's simpler to do it this way than to list all the packages with individual opens statements.
+ */
+open module com.machinezoo.sourceafis.cli {
+	exports com.machinezoo.sourceafis.cli;
+	/*
+	 * JOL needs sun.misc.Unsafe.
+	 */
+	requires jdk.unsupported;
+	requires com.fasterxml.jackson.annotation;
+	requires com.fasterxml.jackson.core;
+	requires com.fasterxml.jackson.databind;
+	requires com.fasterxml.jackson.dataformat.cbor;
+	requires java.desktop;
+	requires com.machinezoo.noexception;
+	requires com.machinezoo.sourceafis;
+	requires it.unimi.dsi.fastutil;
+	requires one.util.streamex;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.slf4j;
+	requires jol.core;
+}
