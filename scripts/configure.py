6e5636d2567a4c2bdd6649a085fd8a3926306edb
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 11:45:10 +0000

    Updated project configuration: upgraded stagean

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,43 +1,40 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import pathlib
-project_directory = lambda: pathlib.Path(__file__).parent.parent
-config_directory = lambda: project_directory().parent/'project-config'
-exec((config_directory()/'src'/'java.py').read_text())
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-project_script_path = __file__
-repository_name = lambda: 'sourceafis-cli-java'
-pretty_name = lambda: 'SourceAFIS CLI for Java'
-pom_subgroup = lambda: 'sourceafis'
-pom_artifact = lambda: 'sourceafis-cli'
-pom_name = lambda: 'SourceAFIS CLI'
-pom_description = lambda: 'Command-line interface for SourceAFIS.'
-inception_year = lambda: 2021
-homepage = lambda: website() + 'cli'
-jdk_version = lambda: 17
-main_class_name = lambda: 'Main'
-stagean_annotations = lambda: True
-md_description = lambda: '''\
-    SourceAFIS CLI for Java is a command-line interface to [SourceAFIS for Java](https://sourceafis.machinezoo.com/java).
-    At the moment, it can benchmark algorithm accuracy, template footprint, and implementation speed.
-    It also includes tools that aid in development of SourceAFIS for Java.
-    Read more on [SourceAFIS CLI](https://sourceafis.machinezoo.com/cli) page.
-'''
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'sourceafis-cli-java'
+    def pretty_name(self): return 'SourceAFIS CLI for Java'
+    def pom_name(self): return 'SourceAFIS CLI'
+    def pom_description(self): return 'Command-line interface for SourceAFIS.'
+    def inception_year(self): return 2021
+    def homepage(self): return self.website() + 'cli'
+    def jdk_version(self): return 17
+    def main_class_name(self): return 'Main'
+    def stagean_annotations(self): return True
+    def project_status(self): return self.stable_status()
+    def md_description(self): return '''\
+        SourceAFIS CLI for Java is a command-line interface to [SourceAFIS for Java](https://sourceafis.machinezoo.com/java).
+        At the moment, it can benchmark algorithm accuracy, template footprint, and implementation speed.
+        It also includes tools that aid in development of SourceAFIS for Java.
+        Read more on [SourceAFIS CLI](https://sourceafis.machinezoo.com/cli) page.
+    '''
+    
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use('com.machinezoo.sourceafis:sourceafis:3.17.1')
+        yield self.use_commons_lang()
+        yield self.use_streamex()
+        yield self.use('org.slf4j:slf4j-simple:1.7.36')
+        # Used to measure memory footprint of SourceAFIS templates.
+        # Causes warnings. Needs replacement.
+        yield self.use('org.openjdk.jol:jol-core:0.16')
+    
+    def documentation_links(self):
+        yield 'SourceAFIS CLI', 'https://sourceafis.machinezoo.com/cli'
+        yield 'SourceAFIS for Java', 'https://sourceafis.machinezoo.com/java'
+        yield 'SourceAFIS overview', 'https://sourceafis.machinezoo.com/'
 
-def dependencies():
-    use('com.machinezoo.sourceafis:sourceafis:3.17.1')
-    use_commons_lang()
-    use_streamex()
-    use('org.slf4j:slf4j-simple:1.7.36')
-    # Used to measure memory footprint of SourceAFIS templates.
-    # Causes warnings. Needs replacement.
-    use('org.openjdk.jol:jol-core:0.16')
-
-def documentation_links():
-    yield 'SourceAFIS CLI', 'https://sourceafis.machinezoo.com/cli'
-    yield 'SourceAFIS for Java', 'https://sourceafis.machinezoo.com/java'
-    yield 'SourceAFIS overview', 'https://sourceafis.machinezoo.com/'
-
-generate()
+Project().generate()
